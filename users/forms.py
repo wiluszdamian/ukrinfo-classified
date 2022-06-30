2afe2d84e363e739dd8a7f7a68ef8cea49b56a3d
Damianovsky <damians.wilusz@gmail.com>
2022-06-30 21:03:25 +0200

    ADD: Edit profile

--- users/forms.py
+++ users/forms.py
@@ -10,4 +10,10 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
         
--- users/models.py
+++ users/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()    
